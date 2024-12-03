ed0851f55cf7591a05c5e6b8994fd8b955a71bdf
M1ko20 <adam.mikolas64@gmail.com>
2024-12-03 09:48:13 +0100

    Initial commit

---
+++ denDruhy/denDruhy/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace denDruhy;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "input.txt";
+        string content = File.ReadAllText(filePath);
+        string[] radek = content.Split("\n");
+        int counter = 0;
+        bool done = true;
+        bool wrong = false;
+        int ascdesc = 0; // 1 = asc, 2 = desc
+        foreach (var s in radek)
+        {
+            ascdesc = 0;
+            done = true;
+            wrong = false;
+            string[] line  = s.Split(" ");
+            int lengthOfLine = line.Length;
+            
+               for (int i = 0; i < lengthOfLine - 1; i++)
+               {
+                       // 1, 2, 4, 2
+                       // 1, 4, 4, 6
+                   if ((int.Parse(line[i]) > int.Parse(line[i+1]) && int.Parse(line[i]) <= int.Parse(line[i + 1]) + 3) && ascdesc != 2) //asc
+                   {
+                       ascdesc = 1;
+                   }
+                   else if ((int.Parse(line[i]) < int.Parse(line[i+1]) && int.Parse(line[i]) >= int.Parse(line[i + 1]) - 3) && ascdesc != 1) //desc
+                   {
+                       ascdesc = 2;
+                   }
+                   else if (!wrong)
+                   {
+                       for (int j = i ; j < lengthOfLine - 1; j++)
+                       {
+                           line[j] = line[j + 1];
+                       }
+                       wrong = true;
+                       i--;
+                       lengthOfLine--;
+                   }
+                   else
+                   {
+                       done = false;
+                       break;
+                   }
+               }
+               if (done) counter++;
+        }
+        //256 too low
+        
+        Console.WriteLine(counter);
+    }
+}
+
+
+/*         if (int.Parse(line[0]) > int.Parse(line[1]) &&  int.Parse(line[0]) <= int.Parse(line[1]) + 3) //desc
+           {
+               for (int i = 1; i < lengthOfLine - 1; i++)
+               {
+                   if (!(int.Parse(line[i]) > int.Parse(line[i+1]) && int.Parse(line[i]) <= int.Parse(line[i + 1]) + 3))
+                   {
+                       if (!wrong)
+                       {
+                           for (int j = i + 1; j < lengthOfLine - 1; j++)
+                           {
+                               line[j] = line[j + 1];
+                           }
+                           wrong = true;
+                           i--;
+                           lengthOfLine--;
+                       }
+                       else {
+                           done = false;
+                           break;
+                       }
+                   }
+               }
+               if (done) counter++;
+           }
+
+           else if (int.Parse(line[0]) < int.Parse(line[1]) &&  int.Parse(line[0]) >= int.Parse(line[1]) - 3) //asc
+           {
+               for (int i = 1; i < lengthOfLine - 1; i++)
+               {
+                   if (!(int.Parse(line[i]) < int.Parse(line[i+1]) && int.Parse(line[i]) >= int.Parse(line[i + 1]) - 3))
+                   {
+                       if (!wrong)
+                       {
+                           for (int j = i + 1; j < lengthOfLine - 1; j++)
+                           {
+                               line[j] = line[j + 1];
+                           }
+                           wrong = true;
+                           i--;
+                           lengthOfLine--;
+                       }
+                       else {
+                           done = false;
+                           break;
+                       }
+                   }
+               }
+               if (done) counter++;
+           }
+       }
+*/
---
+++ denTreti/denTreti/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace denTreti;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "untitled.txt";
+        string content = File.ReadAllText(filePath);
+        int result = 0;
+        bool can = true;
+        for (int i = 0; i < content.Length - 3; i++)
+        {
+            if (content[i] == 'd' && content[i+1] == 'o')
+            {
+                i += 2;
+                if (content[i] == '(' && content[i + 1] == ')')
+                {
+                    can = true;
+                    i += 2;
+                }
+                else if (content[i] == 'n' && content[i + 1] == '\'' && content[i + 2] == 't' && content[i + 3] == '(' && content[i + 4] == ')')
+                {
+                    i += 5;
+                    can = false;
+                }
+                
+            }
+            
+            if (content[i] == 'm' && content [i+1] == 'u' && content[i+2] == 'l' && content[i+3] == '(' && can)
+            {
+                i += 4; 
+                string test = content.Substring(i, 8); 
+                int start = test.IndexOf(',');
+                int end = test.IndexOf(')');
+                if (start == -1 || end == -1 )
+                {
+                    continue;
+                }
+                if ( int.TryParse(test.Substring(0, start ), out int value) && int.TryParse(test.Substring(start+1, end - start - 1), out value))
+                {
+                    Console.WriteLine(test);
+                    result += int.Parse(test.Substring(0, start )) * int.Parse(test.Substring(start + 1, end - start - 1));
+                }
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
